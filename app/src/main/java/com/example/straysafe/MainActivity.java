30a235e150fc8f5195b157a9525bbeb9a8c84359
vishal R <vishal206ani@gmail.com>
2022-06-19 12:38:24 +0530

    Nav-bar and splash screen setup

--- app/src/main/java/com/example/straysafe/MainActivity.java
+++ app/src/main/java/com/example/straysafe/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.straysafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.straysafe.Add.AddFragment;
+import com.example.straysafe.Home.HomeFragment;
+import com.example.straysafe.Profile.ProfileFragment;
+import com.example.straysafe.Store.StoreFragment;
+import com.example.straysafe.Wallet.WalletFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+    HomeFragment homeFragment=new HomeFragment();
+    StoreFragment storeFragment=new StoreFragment();
+    AddFragment addFragment=new AddFragment();
+    WalletFragment walletFragment=new WalletFragment();
+    ProfileFragment profileFragment=new ProfileFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                return true;
+
+            case R.id.store:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, storeFragment).commit();
+                return true;
+
+            case R.id.add:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, addFragment).commit();
+                return true;
+            case R.id.wallet:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, walletFragment).commit();
+                return true;
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/straysafe/SplashScreenActivity.java
+++ app/src/main/java/com/example/straysafe/SplashScreenActivity.java
@@ -2,13 +2,32 @@ package com.example.straysafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreenActivity.this, MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
